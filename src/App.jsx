78be384099fccbbfbb841e5cf14078edd09aebe0
Gabriela Alves <gabriela_alves1105@hotmail.com>
2022-12-08 15:36:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './app.module.css';
+
+function App() {
+
+  return (
+    <header className={styles.header}>
+      <h1>Hello,world!</h1>
+    </header>
+  )
+}
+
+export default App
